c78e903c5932b2f57bd78633e5ef62c6cf9f3b57
zhsir123 <zhsir123@gmail.com>
2017-09-09 15:09:13 +0900

    기초 기능 및 ui

---
+++ WordRelayGame/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WordRelayGame
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            wordLabel.Content = "섹스";
+        }
+
+        private void Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            
+            if (e.Key == Key.Enter)
+            {
+                var input = tb.Text;
+                if (CheckWordLabel(input[0]))
+                {
+                    wordLabel.Content = input;
+                }
+
+                tb.Clear();
+            }
+        }
+
+        private Boolean CheckWordLabel(char ch)
+        {
+            String label = wordLabel.Content.ToString().Trim();
+
+            return (label == "") || (label[label.Length - 1] == ch);
+        }
+    }
+}
